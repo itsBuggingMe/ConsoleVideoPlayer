b5e28094936e5e5a91755e458c4c5d041a04559d
itsBuggingMe <lohj002@gmail.com>
2024-01-11 22:57:04 -0500

    color imoprements

--- ba2/Program.cs
+++ ba2/Program.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Drawing;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace ba2
 {
@@ -10,6 +11,7 @@ namespace ba2
         static void Main(string[] args)
         {
             Console.WriteLine("Fullscreen console. Enter file path or leave empty for bad apple");
+            FConsole.Initialize("Console Media Player");
             string option = Console.ReadLine();
 
             using (var videoFrameReader = new VideoFrameReader(string.IsNullOrEmpty(option) ? $"{Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().FullName)}\\Bad Apple!! - Full Version w_video [Lyrics in Romaji, Translation in English].mp4" : option))
@@ -39,19 +41,37 @@ namespace ba2
                             {
                                 int screenx = (i * widthM);
                                 int screeny = (j * heightM);
+                                int index = (screeny * image.Width + screenx) * 3;
 
-                                _sb.Append(data[(screeny * image.Width + screenx) * 3] < 128 ? ' ' : '█');
+                                FConsole.SetChar(
+                                    (short)i, 
+                                    (short)j, 
+                                    new PixelValue(
+                                        FromColor(data[index],
+                                            data[index + 1], 
+                                            data[index + 2])
+                                        )
+                                    );
                             }
                         }
 
-                        Console.SetCursorPosition(0, 0);
-                        Console.Write(_sb);
-                        _sb.Clear();
+                        data = null;
+                        GC.Collect();
 
-                        Thread.Sleep(50);
+                        FConsole.DrawBuffer();
                     }
                 }
             }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ConsoleColor FromColor(byte r, byte g, byte b)
+        {
+            int index = (r > 128 | g > 128 | b > 128) ? 8 : 0; // Bright bit
+            index |= (r > 64) ? 4 : 0; // Red bit
+            index |= (r > 64) ? 2 : 0; // Green bit
+            index |= (b > 64) ? 1 : 0; // Blue bit
+            return (ConsoleColor)index;
         }
     }
 }
