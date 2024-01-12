cd3216b0a242d136ba493d6d850319d6af3cb096
1017251 <1017251@CRN-323E-13-D>
2024-01-12 14:16:35 -0500

    better video player

--- ba2/Program.cs
+++ ba2/Program.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Drawing;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Spectre.Console;
 
 namespace ba2
 {
@@ -11,22 +12,27 @@ namespace ba2
         static void Main(string[] args)
         {
             Console.WriteLine("Fullscreen console. Enter file path or leave empty for bad apple");
-            FConsole.Initialize("Console Media Player");
-            string option = Console.ReadLine();
+
+            //string option = Console.ReadLine();
+            string option = @"G:\My Drive\APCSA\testVideo.mp4";
+            option = @"G:\My Drive\APCSA\videoplayback.mp4";
 
             using (var videoFrameReader = new VideoFrameReader(string.IsNullOrEmpty(option) ? $"{Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().FullName)}\\Bad Apple!! - Full Version w_video [Lyrics in Romaji, Translation in English].mp4" : option))
             {
-                StringBuilder _sb = new StringBuilder();
-
                 videoFrameReader.Read();
                 var firstFrame = videoFrameReader.GetFrame();
                 int width = firstFrame.Width;
                 int height = firstFrame.Height;
 
+                Console.Clear();
+
+
                 while (videoFrameReader.Read())
                 {
-                    int consoleBufferWidth = Console.BufferWidth;
-                    int consoleBufferHeight = Console.BufferHeight;
+                    int consoleBufferWidth = Console.WindowWidth;
+                    int consoleBufferHeight = Console.WindowHeight + 2;
+
+                    var canvas = new Canvas(consoleBufferWidth >> 1, consoleBufferHeight);
 
                     int widthM = width / consoleBufferWidth;
                     int heightM = height / consoleBufferHeight;
@@ -35,64 +41,33 @@ namespace ba2
                     {                        
                         byte[] data = image.ToPixelData();
 
-                        for (int j = 0; j < consoleBufferHeight; j++)
+                        for (int j = 0; j < consoleBufferHeight - 1; j++)
                         {
-                            for (int i = 0; i < consoleBufferWidth; i++)
+                            for (int i = 0; i < consoleBufferWidth - 1; i++)
                             {
                                 int screenx = (i * widthM);
                                 int screeny = (j * heightM);
                                 int index = (screeny * image.Width + screenx) * 3;
-
-                                FConsole.SetChar(
-                                    (short)i, 
-                                    (short)j, 
-                                    new PixelValue(
-                                        ClosestConsoleColor(data[index],
-                                            data[index + 1], 
-                                            data[index + 2])
-                                        )
-                                    );
+                                    
+                                canvas.SetPixel(i >> 1,j, new Spectre.Console.Color(
+                                    data[index + 2],
+                                    data[index + 1],
+                                    data[index]
+                                    ));
                             }
                         }
 
                         data = null;
                         GC.Collect();
 
-                        FConsole.DrawBuffer();
+                        //Console.Clear();
+
+                        Console.SetCursorPosition(0,0);
+                        AnsiConsole.Write(canvas);
+                        //Thread.Sleep(50);
                     }
                 }
             }
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ConsoleColor FromColor(byte r, byte g, byte b)
-        {
-            int index = (r > 128 | g > 128 | b > 128) ? 8 : 0; // Bright bit
-            index |= (r > 64) ? 4 : 0; // Red bit
-            index |= (r > 64) ? 2 : 0; // Green bit
-            index |= (b > 64) ? 1 : 0; // Blue bit
-            return (ConsoleColor)index;
-        }
-
-        static ConsoleColor ClosestConsoleColor(byte r, byte g, byte b)
-        {
-            ConsoleColor ret = 0;
-            double rr = r, gg = g, bb = b, delta = double.MaxValue;
-
-            foreach (ConsoleColor cc in Enum.GetValues(typeof(ConsoleColor)))
-            {
-                var n = Enum.GetName(typeof(ConsoleColor), cc);
-                var c = System.Drawing.Color.FromName(n == "DarkYellow" ? "Orange" : n); // bug fix
-                var t = Math.Pow(c.R - rr, 2.0) + Math.Pow(c.G - gg, 2.0) + Math.Pow(c.B - bb, 2.0);
-                if (t == 0.0)
-                    return cc;
-                if (t < delta)
-                {
-                    delta = t;
-                    ret = cc;
-                }
-            }
-            return ret;
         }
     }
 }
