ba2ecfcde36f066e6c3f52f038df39372acf8d02
itsBuggingMe <lohj002@gmail.com>
2024-01-11 22:12:31 -0500

    Add project files.

---
+++ BaddApple2/Program.cs
@@ -0,0 +1,43 @@
+﻿using GleamTech.VideoUltimate;
+using System.Drawing;
+using System.Text;
+
+namespace BaddApple2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fullscreen");
+            Console.ReadLine();
+
+            using (var videoFrameReader = new VideoFrameReader(@"C:\Users\Jason\Downloads\Bad Apple!! - Full Version w_video [Lyrics in Romaji, Translation in English].mp4"))
+            {
+                var test = videoFrameReader.GetFrame();
+                int width = test.Width;
+                int height = test.Height;
+
+                while(videoFrameReader.Read())
+                {
+                    using(var image = videoFrameReader.GetFrame())
+                    {
+                        Bitmap bmp = (Bitmap)image;
+                        StringBuilder _sb = new StringBuilder();
+                        for(int i = 0; i < Console.BufferWidth; i++)
+                        {
+                            for (int j = 0; j < Console.BufferHeight; j++)
+                            {
+                                Color colorAt = bmp.GetPixel((i / Console.BufferWidth) * width, (i / Console.BufferHeight) * height);
+                                _sb.Append(colorAt.R > 128 ? ' ' : '█');
+                            }
+                        }
+
+                        Console.SetCursorPosition(0,0);
+                        Console.Clear();
+                        Console.Write(_sb);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ba2/Program.cs
@@ -0,0 +1,57 @@
+﻿using GleamTech.VideoUltimate;
+using System.Text;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+namespace ba2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fullscreen console. Enter file path or leave empty for bad apple");
+            string option = Console.ReadLine();
+
+            using (var videoFrameReader = new VideoFrameReader(string.IsNullOrEmpty(option) ? $"{Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().FullName)}\\Bad Apple!! - Full Version w_video [Lyrics in Romaji, Translation in English].mp4" : option))
+            {
+                StringBuilder _sb = new StringBuilder();
+
+                videoFrameReader.Read();
+                var firstFrame = videoFrameReader.GetFrame();
+                int width = firstFrame.Width;
+                int height = firstFrame.Height;
+
+                while (videoFrameReader.Read())
+                {
+                    int consoleBufferWidth = Console.BufferWidth;
+                    int consoleBufferHeight = Console.BufferHeight;
+
+                    int widthM = width / consoleBufferWidth;
+                    int heightM = height / consoleBufferHeight;
+
+                    using (var image = videoFrameReader.GetFrame())
+                    {                        
+                        byte[] data = image.ToPixelData();
+
+                        for (int j = 0; j < consoleBufferHeight; j++)
+                        {
+                            for (int i = 0; i < consoleBufferWidth; i++)
+                            {
+                                int screenx = (i * widthM);
+                                int screeny = (j * heightM);
+
+                                _sb.Append(data[(screeny * image.Width + screenx) * 3] < 128 ? ' ' : '█');
+                            }
+                        }
+
+                        Console.SetCursorPosition(0, 0);
+                        Console.Write(_sb);
+                        _sb.Clear();
+
+                        Thread.Sleep(50);
+                    }
+                }
+            }
+        }
+    }
+}
